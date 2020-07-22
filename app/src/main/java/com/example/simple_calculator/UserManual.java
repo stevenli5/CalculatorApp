26e3b526120472af7e670e0d5bb424aa46b999e3
Steven Li <stevenli.581@gmail.com>
2020-07-22 17:54:15 -0400

    Version 2.0 final upload

--- app/src/main/java/com/example/simple_calculator/UserManual.java
+++ app/src/main/java/com/example/simple_calculator/UserManual.java
@@ -1,14 +1,11 @@
 package com.example.simple_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class UserManual extends AppCompatActivity {
     Button buttonBack;
